3ce2c3286d7dc97f1718f4c19b451b371da80415
Robert Cloa <8luM3CT0@users.noreply.github.com>
2022-02-27 20:28:06 -0800

    new commit: commit again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    stripe_publish_key: 'pk_test_51JA5aeJx9wXnBmy1arwyDQL4KeuVsSktOPSothxhWHkXnuSrGHssCnlajpHzTg7pWprqFrF4HqsXAYNeVskDYLmU00Boqpmnjy',
+    stripe_secret_key: 'sk_test_51JA5aeJx9wXnBmy1NNm2XxTCBPnMqmi7Mv2aHQaRhxLvbCsNI8kVJjcp4x9yoXClw1Uuv6wx7s10gQw9U2MKBbwM00fgBfVIJo'
+  }
 }
 
 module.exports = nextConfig
